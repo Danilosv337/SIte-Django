48915ee69ebbc07e6e584c4883081a58b4cba4f1
Danilosv337 <danilo_santos.vieira@hotmail.com>
2023-02-21 09:12:45 -0300

    add menu e reform index

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 
 class Projetos(models.Model):
+    
     nome = models.CharField(max_length=200,null=False,blank=False)
     imagem = models.ImageField(upload_to="ImagemProjetos/%Y/%m/%d/",blank=True)
     legenda = models.CharField(max_length=255,null=False,blank=False)
-    descricao = models.TextField(null=False,blank=True)
+    descricao = models.TextField(null=False,blank=True,verbose_name='Descrição')
     link_projeto = models.CharField(max_length=255,null=False,blank=True)
 
     def __str__(self):
         return f'{self.nome}'
+
+    class Meta:
+        verbose_name = 'Projeto'
+        verbose_name_plural = 'Projetos'
